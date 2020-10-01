8c4c704e7a1efdffba3b3e762384fd42aedfb4f1
Jonathan Gomez <Jonathan Gomez@LAPTOP-G8AGCN46>
2020-09-30 20:38:28 -0500

    Ajuste de prueba

--- ProgramasCorteII/ProgramasCorteII/MenuProgramas.cs
+++ ProgramasCorteII/ProgramasCorteII/MenuProgramas.cs
@@ -13,7 +13,7 @@ namespace ProgramasCorteII
                 Console.Clear();
                 Console.WriteLine("UNIVERSIDAD ECCI");
                 Console.WriteLine("MENU DE PROGRAMAS - VISUAL C SHARP");
-                Console.WriteLine("Nomina de Empleados");
+                Console.WriteLine("1. Nomina de Empleados");
                 Console.WriteLine("2. Sistema de Notas - Universidad Ecci");
                 Console.WriteLine("3. Operaciones Matematicas");
                 Console.WriteLine("4. Evaluacion y Analisis de Respuestas");
@@ -22,7 +22,7 @@ namespace ProgramasCorteII
                 Console.WriteLine("7. Copia de Seguridad");
                 Console.WriteLine("8. Caracteristicas de Una Serie de Numeros");
                 Console.WriteLine("9. Estadistica de Pesos");
-                Console.WriteLine(". Funciones Matematicas");
+                Console.WriteLine("10. Funciones Matematicas");
                 Console.WriteLine("Seleccione el numero del programa a ejecutar");
                 opcion = int.Parse(Console.ReadLine());
                 switch (opcion)
