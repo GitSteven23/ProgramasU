a6d4a18012d60b0f3e00ed2eca37db6b2fa77a79
Steven Chacón <steven2010_13@hotmail.com>
2020-10-06 22:14:59 -0500

    Commit - Ajustes

--- ProgramasCorteII/ProgramasCorteII/ProduccionEmpleado.cs
+++ ProgramasCorteII/ProgramasCorteII/ProduccionEmpleado.cs
@@ -1,10 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.Design;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Xml.Schema;
 
 namespace ProgramasCorteII
 {
@@ -25,25 +20,25 @@ namespace ProgramasCorteII
                 Console.WriteLine("Produccion Empleado");
                 Console.WriteLine("==============================================");
 
-                                for (int i = 1; i < dias.Length ; i++)
+                for (int i = 1; i < dias.Length; i++)
                 {
-                    Console.WriteLine("Digite la productivdad del dia :"+i);
+                    Console.WriteLine("Digite la productivdad del dia :" + i);
                     dias[i] = int.Parse(Console.ReadLine());
 
                 }
 
                 total = (dias.Sum() / 6);
-                
+
                 if (total < 100)
                 {
-                   Console.WriteLine("La productivdad del trabajador es de : " + total);
-                   Console.WriteLine("El trabajador no merece una bonificacion");
+                    Console.WriteLine("La productivdad del trabajador es de : " + total);
+                    Console.WriteLine("El trabajador no merece una bonificacion");
 
                 }
                 else
                 {
-                   Console.WriteLine("La productivdad del trabajador es de : " + total);
-                   Console.WriteLine("El trabajador merece una bonificacion");
+                    Console.WriteLine("La productivdad del trabajador es de : " + total);
+                    Console.WriteLine("El trabajador merece una bonificacion");
                 }
 
                 Console.WriteLine("Desea realizar otro cálculo: S/N");
